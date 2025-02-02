e0e0347f0729734283146989c3830143087b0b64
nyu-jesusp <jp7831@nyu.edu>
2025-02-01 20:37:00 -0800

    Update gettingStarted.py
    correct tcp/ip model answers and changed decode to yes

--- gettingStarted.py
+++ gettingStarted.py
@@ -14,7 +14,7 @@ def welcome_assignment_answers(question):
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
-        answer = "No"
+        answer = "Yes"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = "No"
     elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
@@ -22,9 +22,9 @@ def welcome_assignment_answers(question):
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = "7"
+        answer = "4"
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-        answer = "3"
+        answer = "2"
     else:
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
